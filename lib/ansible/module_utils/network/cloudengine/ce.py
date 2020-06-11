1d723f1fdc9fa7fdc90d75efcdaaba6485d4724e
yanzhangi <51999930+yanzhangi@users.noreply.github.com>
2020-06-11 08:54:13 +0800

    Revert "Modify the way of parsing NETCONF XML message in ce.py (#69571)"
    This reverts commit e78c8be3d19c56d41ae77df32d8f92d3d4826cf5.

--- lib/ansible/module_utils/network/cloudengine/ce.py
+++ lib/ansible/module_utils/network/cloudengine/ce.py
@@ -367,7 +367,7 @@ def get_nc_next(module, xml_str):
             except ConnectionError:
                 break
     if result is not None:
-        return to_string(to_xml(result))
+        return etree.tostring(result)
     return result
 
 
